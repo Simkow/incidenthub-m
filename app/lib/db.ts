05316c61ad54ad8e89b99c3ddec22d044b869e22
simkow <simkow03@gmail.com>
2026-02-17 20:35:02 +0100

    fix: update DATABASE_URL handling to provide a default connection string

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -4,6 +4,6 @@ if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL env var is not set');
 }
 
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon(process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_D0arJmP6cHUw@ep-delicate-tooth-agfl0bpg-pooler.c-2.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require');
 
 export { sql };
