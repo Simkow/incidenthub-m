259e21d495073fbe7926679822ed5b158a68370b
simkow <simkow03@gmail.com>
2026-03-04 20:59:49 +0100

    feat: add Footer and Presentation components

--- app/home/Navbar.tsx
+++ app/home/Navbar.tsx
@@ -75,47 +75,39 @@ export const Navbar: React.FC = () => {
     <div className="w-full fixed px-6 md:px-12 lg:px-56 py-4 bg-[#090909]/70 text-gray-400 flex flex-col md:flex-row items-center md:justify-between gap-4 heading border-b border-white/30 z-50 backdrop-blur-sm">
       <div
         onClick={() => router.push("/")}
-        className="flex gap-4 items-center justify-center shrink-0 cursor-pointer"
+        className="flex gap-2 items-center justify-center shrink-0 cursor-pointer"
       >
-        <Image src={Logo} alt="IncidentHub Logo" className="h-12 w-12" />
+        <Image src={Logo} alt="IncidentHub Logo" className="h-8 w-8" />
         <span className="font-medium text-lg text-transparent bg-linear-to-r from-neutral-200 to-neutral-300 bg-clip-text">
           IncidentHub
         </span>
       </div>
-      <div className="flex gap-4 md:gap-8 text-sm md:text-base flex-wrap justify-center">
-        {/* <Link
-          href="/"
-          className="hover:text-neutral-300 transition-all duration-300 opacity-50"
-        >
-          {t("navbar.product")}
-        </Link> */}
+      <div className="flex items-center gap-3">
         <Link
           href={dashboardHref}
           onClick={guardDashboardClick}
-          className=" hover:text-neutral-300 transition-all duration-300"
+          className=" hover:text-neutral-300 transition-all duration-300 mr-4"
         >
           {t("navbar.dashboard")}
         </Link>
         <Link
           href="/contact"
-          className=" hover:text-neutral-300 transition-all duration-300 opacity-50"
+          className=" hover:text-neutral-300 transition-all duration-300 opacity-50 mr-4"
         >
           {t("navbar.contact")}
         </Link>
-      </div>
-      <div className="flex items-center gap-3">
         <LocaleToggle />
         {!token ? (
           <div className="flex gap-3 md:gap-4 text-sm md:text-base flex-wrap justify-center">
             <Link
               href="/login"
-              className="font-base hover:text-neutral-300 px-8 py-1 bg-black/40 hover:bg-black/50 rounded-xl border border-white/50 transition-all duration-300"
+              className="font-base hover:text-neutral-300 px-8 py-1 bg-black/40 hover:bg-black/50 rounded-xl border border-neutral-800 transition-all duration-300"
             >
               {t("navbar.login")}
             </Link>
             <Link
               href="/login"
-              className="font-base hover:text-black text-black/90 px-8 py-1 bg-white/90 hover:bg-white rounded-xl border border-white/50 transition-all duration-300"
+              className="font-base hover:text-black text-black/90 px-8 py-1 bg-white/90 hover:bg-white rounded-xl border border-neutral-800 transition-all duration-300"
             >
               {t("navbar.openApp")}
             </Link>
@@ -128,14 +120,14 @@ export const Navbar: React.FC = () => {
                 e.preventDefault();
                 void handleLogout();
               }}
-              className="font-base hover:text-neutral-300 px-8 py-1 bg-black/40 hover:bg-black/50 rounded-xl border border-white/50 transition-all duration-300"
+              className="font-base hover:text-neutral-300 px-8 py-1 bg-black/40 hover:bg-black/50 rounded-xl border border-neutral-800 transition-all duration-300"
             >
               {t("navbar.logout")}
             </Link>
             <Link
               href={dashboardHref}
               onClick={guardDashboardClick}
-              className="font-base hover:text-black text-black/90 px-8 py-1 bg-white/90 hover:bg-white rounded-xl border border-white/50 transition-all duration-300"
+              className="font-base hover:text-black text-black/90 px-8 py-1 bg-white/90 hover:bg-white rounded-xl border border-neutral-800 transition-all duration-300"
             >
               {t("navbar.openApp")}
             </Link>
---
+++ app/home/Presentation.tsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+
+export const Presentation = () => {
+    
+    return(
+        <main>
+
+        </main>
+    );
+}
