2639b3b21daad9f0c6c717f127d7e97a49df0c3d
simkow <simkow03@gmail.com>
2026-03-05 11:32:42 +0100

    fix: correct import statement formatting and ensure consistent use of semicolons

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,15 @@
 
 import { Navbar } from "./home/Navbar";
 import { Hero } from "./home/Hero";
-import { Presentation } from "./home/Presentation"
+import { Presentation } from "./home/Presentation";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [])
+
   return (
     <div className="w-full h-full bg-[#090909]">
       <Navbar />
